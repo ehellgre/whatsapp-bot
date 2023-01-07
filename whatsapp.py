f4e38421232f618a6f5239b4a7559cd9056afdea
ehellgre <emil.hellgren@edu.lapinamk.fi>
2023-01-07 15:50:23 +0200

    Dropping file

--- whatsapp.py
+++ whatsapp.py
@@ -3,13 +3,13 @@ import pywhatkit
 phone_number = input("Enter phone number: ")
 
 # Phonenumber where to send the msg, Message, Time_Hours, Time_Mins, Wait time in secs, Closing tab, How many secs until tab closes
-pywhatkit.sendwhatmsg(phone_number, "Test", 15, 33, 15, True, 2)
+pywhatkit.sendwhatmsg(phone_number, "Test", 15, 47, 15, True, 2)
 
 # Group message
 group_id = input("Enter group id: ")
 
 # Group id, message, time_hours, time_mins
-pywhatkit.sendwhatmsg_to_group(group_id, "Test Group", 15, 33)
+pywhatkit.sendwhatmsg_to_group(group_id, "Test Group", 15, 47)
 
 
 
